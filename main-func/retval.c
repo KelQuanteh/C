c180c8760eff74f029b49eb59e8ddfce6fd9ba01
Kelvin <123701224+KelQuanteh@users.noreply.github.com>
2023-09-03 09:20:43 -0300

    Update retval.c

--- main-func/retval.c
+++ main-func/retval.c
@@ -1,19 +1,21 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <ctype.h>
-#include <stdlib.h> 
+#include <stdlib.h>
 #include <unistd.h>
 
-int val; 
+int val; // Global variable to store the count of command-line options
 
-int main(int argc, char** argv){
-	int count = 0;
+int main(int argc, char** argv) {
+    int count = 0; // Local variable to store the count of command-line options
 
-	for(int i = 1; i < argc; i++){
-		if(argv[i][0] == '-')
-			count++;
-	}
-	
-	val = count;
-	
-	return (val); 
+    // Loop through the command-line arguments starting from the second argument (argv[1])
+    for (int i = 1; i < argc; i++) {
+        // Check if the argument starts with a hyphen '-' indicating it's an option
+        if (argv[i][0] == '-')
+            count++; // Increment the count for each option encountered
+    }
+
+    val = count; // Store the total count of options in the global variable 'val'
+
+    return val; // Return the count of options as the exit status of the program
 }

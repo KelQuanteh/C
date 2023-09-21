5abc5a4d4cc55ed16308758ffe1fcba27e619f4e
Arch Mettatron <kkq20@inf.ufpr.br>
2023-09-21 14:56:19 -0300

    update union with comments

--- union.c
+++ union.c
@@ -5,11 +5,12 @@ typedef union uniao_t
 {
     char caracteres[21];
     int vet[6];
-}uniao;
+} uniao;
 
-int main(){
+int main() {
     uniao exemplo;
 
+    // Assigning values to the integer array
     exemplo.vet[0] = '2';
     exemplo.vet[1] = '4';
     exemplo.vet[2] = '6';
@@ -17,6 +18,7 @@ int main(){
     exemplo.vet[4] = '9';
     exemplo.vet[5] = '1';
 
+    // Assigning values to the character array
     exemplo.caracteres[0] = 'T';
     exemplo.caracteres[1] = 'u';
     exemplo.caracteres[2] = 'd';
@@ -27,14 +29,13 @@ int main(){
     exemplo.caracteres[7] = 'm';
     exemplo.caracteres[8] = '?';
 
-
-    for (int i = 0; i < sizeof(exemplo.caracteres); i++)
-    {
-        printf("caracter[%d]: %c \n", i+1, exemplo.caracteres[i]);
+    // Printing individual characters from the character array
+    for (int i = 0; i < sizeof(exemplo.caracteres); i++) {
+        printf("caracter[%d]: %c \n", i + 1, exemplo.caracteres[i]);
     }
 
+    // Printing the entire character array as a string
     printf("%s \n", exemplo.caracteres);
-    
 
     return 0;
 }

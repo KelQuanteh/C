f7208fa2aafafb0da894152f0b58c96511da58a3
Hiram-Abiff <kevinomalik@gmail.com>
2025-06-05 13:06:33 -0300

    updated to check errors

--- threads.c
+++ threads.c
@@ -15,8 +15,15 @@ int main(){
     pthread_t t1, t2; 
 
     //initialize threads
-    pthread_create(&t1, NULL, &routine, NULL);
-    pthread_create(&t2, NULL, &routine, NULL);
+    if (pthread_create(&t1, NULL, &routine, NULL)){
+        perror("Unable to create thread\n"); 
+        exit(1); 
+    }
+    
+    if (pthread_create(&t2, NULL, &routine, NULL)){
+        perror("Unable to create thread\n"); 
+        exit(1); 
+    }
 
     //get threads
     pthread_join(t1, NULL); 
